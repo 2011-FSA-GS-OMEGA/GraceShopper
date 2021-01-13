c75369a3c32cb95f6076b130bbc07fee3a16ad7c
Aram <aram_martin@alumni.brown.edu>
2021-01-13 16:02:09 -0600

    feat: AllProducts has rendered using Product Card template && link for AllProducts page has been included in Navbar

--- server/db/db.js
+++ server/db/db.js
@@ -3,8 +3,10 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 // `postgres://localhost:5432/${databaseName}`
+// `postgres://postgres:glen@localhost/${databaseName}`
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    `postgres://postgres:glen@localhost/${databaseName}`,
   {
     logging: false
   }
