8f913e1ea3009b44295cc56eca3bad62e81fd45e
Aram <aram_martin@alumni.brown.edu>
2021-01-14 09:51:08 -0600

    feat: adjusted Link element in ProductCard component so that ONLY the name is clickable

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -4,19 +4,19 @@ import {Link} from 'react-router-dom'
 const ProductCard = ({product}) => {
   return (
     <div className="card">
-      <Link to={`/products/${product.id}`}>
-        <div className="card-body">
-          <img className="card-image" src={product.imageUrl} />
-          <div className="card-stats">
+      <div className="card-body">
+        <img className="card-image" src={product.imageUrl} />
+        <div className="card-stats">
+          <Link to={`/products/${product.id}`}>
             <h2>{product.name}</h2>
-            <ul>
-              <li>Price: ${product.price}</li>
-              <li>Type: {product.type}</li>
-              <li>Status: {product.status}</li>
-            </ul>
-          </div>
+          </Link>
+          <ul>
+            <li>Price: ${product.price}</li>
+            <li>Type: {product.type}</li>
+            <li>Status: {product.status}</li>
+          </ul>
         </div>
-      </Link>
+      </div>
     </div>
   )
 }
