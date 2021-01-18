041328867d26e94cedd91430c177453a978ad1f8
Stephen Ro <stephenkro@gmail.com>
2021-01-17 19:56:06 -0500

    style(changes to Cart, Navbar and Single Product styles color and spacing)

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,7 +6,13 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div id="navbar-header">
-    <h1>Jellybeans and Shoes</h1>
+    <div className="navbar-title">
+      <img
+        className="jellybean"
+        src="https://rlv.zcache.com/cutie_cartoon_red_jelly_bean_vinyl_sticker-r3c275299c36c4187b0f782ac3465db98_08m3m_704.jpg?rlvnet=1"
+      />
+      <h1>Jellybeans and Shoes</h1>
+    </div>
     <nav className="navbar">
       {isLoggedIn ? (
         <div>
